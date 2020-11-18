19e9520c1cc428b7b13b78b137eafef9cdc426fb
Nan Wang <38221905+nanw0321@users.noreply.github.com>
2020-11-19 00:47:21 +0800

    Update: alignment simulation by changing the roc of the thinnest lens

--- Code_alignment/Functions.py
+++ Code_alignment/Functions.py
@@ -189,3 +189,170 @@ def get_spectrum(pulse, image_name, x_pos=0, y_pos=0, integrated=False):
     
     # [eV], normalized intensity [simulated], [Gaussian Fit]
     return pulse.energy - pulse.E0, y_data/np.max(y_data), gauss_plot
+
+def find_t_center(pulse, image_name):
+    intensity = np.sum(np.abs(pulse.time_stacks[image_name]), axis=(0,1))
+    tcenter = pulse.t_axis[np.argmax(intensity)]
+    return tcenter
+
+
+''' plot '''
+def plot_profile(pulse, image_name, part_name):
+    # minima and maxima of the field of view (in microns) for imshow extent
+    minx = np.round(np.min(pulse.x[image_name]) * 1e6)
+    maxx = np.round(np.max(pulse.x[image_name]) * 1e6)
+    miny = np.round(np.min(pulse.y[image_name]) * 1e6)
+    maxy = np.round(np.max(pulse.y[image_name]) * 1e6)
+
+    # generate the figure
+    profile = np.sum(np.abs(pulse.time_stacks[image_name])**2, axis=2)
+
+    # show the 2D profile
+    plt.imshow(np.flipud(profile),
+                      extent=(minx, maxx, miny, maxy), cmap=plt.get_cmap('gnuplot'),
+                            clim=(0,np.max(profile)))
+    # label coordinates
+    plt.xlabel('X coordinates (microns)')
+    plt.ylabel('Y coordinates (microns)')
+    plt.title('Profile {}'.format(part_name))
+
+def plot_Tslice(pulse, image_name, part_name, dim='x', slice_pos=0, shift=None):
+    # minima and maxima of the field of view (in microns) for imshow extent
+    minx = np.round(np.min(pulse.x[image_name]) * 1e6)
+    maxx = np.round(np.max(pulse.x[image_name]) * 1e6)
+    miny = np.round(np.min(pulse.y[image_name]) * 1e6)
+    maxy = np.round(np.max(pulse.y[image_name]) * 1e6)
+    min_t = np.min(pulse.t_axis)
+    max_t = np.max(pulse.t_axis)
+
+    # horizontal slice
+    if dim == 'x':
+        # slice index
+        N = pulse.x[image_name].size
+        dx = (maxx - minx) / N
+        index = int((slice_pos - minx) / dx)
+        profile = np.abs(pulse.time_stacks[image_name][index, :, :]) ** 2
+        extent = (min_t, max_t, minx, maxx)
+        ylabel = 'X coordinates (microns)'
+        aspect_ratio = (max_t - min_t) / (maxx - minx)
+        title = u'Time Slice: Y = %d \u03BCm %s' % (slice_pos, part_name)
+    # vertical slice
+    elif dim == 'y':
+        # slice index
+        N = pulse.y[image_name].size
+        dx = (maxy - miny) / N
+        index = int((slice_pos - miny) / dx)
+        profile = np.abs(pulse.time_stacks[image_name][:, index, :]) ** 2
+        extent = (min_t, max_t, miny, maxy)
+        ylabel = 'Y coordinates (microns)'
+        aspect_ratio = (max_t - min_t) / (maxy - miny)
+        title = u'Time Slice: X = %d \u03BCm %s' % (slice_pos, part_name)
+    else:
+        profile = np.zeros((256, 256))
+        extent = (0, 0, 0, 0)
+        ylabel = ''
+        aspect_ratio = 1
+
+    # normalize
+    profile = profile/np.max(profile)
+
+    if shift is not None:
+        profile = np.roll(profile, int(shift/pulse.deltaT), axis=1)
+
+    # show the 2D profile
+    plt.imshow(np.flipud(profile), aspect=aspect_ratio,
+                      extent=extent, cmap=plt.get_cmap('gnuplot'))
+    # label coordinates
+    plt.xlabel('Time (fs)')
+    plt.ylabel(ylabel)
+    plt.title(title)
+
+def plot_Eslice(self, image_name, part_name, dim='x', slice_pos=0, image_type='intensity'):
+    # minima and maxima of the field of view (in microns) for imshow extent
+    minx = np.round(np.min(self.x[image_name]) * 1e6)
+    maxx = np.round(np.max(self.x[image_name]) * 1e6)
+    miny = np.round(np.min(self.y[image_name]) * 1e6)
+    maxy = np.round(np.max(self.y[image_name]) * 1e6)
+    min_E = np.min(self.energy) - self.E0
+    max_E = np.max(self.energy) - self.E0
+
+    # horizontal slice
+    if dim == 'x':
+        # slice index
+        N = self.x[image_name].size
+        dx = (maxx - minx) / N
+        index = int((slice_pos - minx) / dx)
+
+        profile = np.abs(self.energy_stacks[image_name][index, :, :]) ** 2
+        profile = profile / np.max(profile)
+        if image_type == 'phase':
+            mask = (profile > 0.01 * np.max(profile)).astype(float)
+            profile = unwrap_phase(np.angle(self.energy_stacks[image_name][index, :, :])) * mask
+            cmap = plt.get_cmap('jet')
+        else:
+            cmap = plt.get_cmap('gnuplot')
+        extent = (min_E, max_E, minx, maxx)
+        ylabel = 'X coordinates (microns)'
+        aspect_ratio = (max_E - min_E) / (maxx - minx)
+        title = u'Energy Slice: Y = %d \u03BCm %s' % (slice_pos, part_name)
+    # vertical slice
+    elif dim == 'y':
+        # slice index
+        N = self.y[image_name].size
+        dx = (maxy-miny)/N
+        index = int((slice_pos-miny)/dx)
+
+        profile = np.abs(self.energy_stacks[image_name][:, index, :]) ** 2
+        profile = profile / np.max(profile)
+        if image_type == 'phase':
+            mask = (profile > 0.01 * np.max(profile)).astype(float)
+            profile = unwrap_phase(np.angle(self.energy_stacks[image_name][:, index, :])) * mask
+            cmap = plt.get_cmap('jet')
+        else:
+            cmap = plt.get_cmap('gnuplot')
+        extent = (min_E, max_E, miny, maxy)
+        ylabel = 'Y coordinates (microns)'
+        aspect_ratio = (max_E-min_E)/(maxy-miny)
+        title = u'Energy Slice: X = %d \u03BCm %s' % (slice_pos, part_name)
+    else:
+        profile = np.zeros((256,256))
+        extent = (0, 0, 0, 0)
+        ylabel = ''
+        aspect_ratio = 1
+        title = ''
+
+    # show the 2D profile
+    plt.imshow(np.flipud(profile), aspect=aspect_ratio,
+                      extent=extent, cmap=cmap)
+    # label coordinates
+    plt.xlabel('Energy (eV)')
+    plt.ylabel(ylabel)
+    # ax_profile.set_title('%s Energy Slice' % image_name)
+    plt.title(title)
+
+def plot_profiles_all(pulse, im_names, part_names):
+    n_img = len(part_names)
+    plt.figure(figsize=(5.4*n_img+2, 5.4))
+    for i in range(n_img):
+        im_name = im_names[i]
+        part_name = part_names[i]
+        plt.subplot(1,n_img,i+1)
+        plot_profile(pulse, im_name, part_name)
+
+def plot_Tslice_all(pulse, im_names, part_names, dim, shift):
+    n_img = len(part_names)
+    plt.figure(figsize=(5.4*n_img+2, 5.4))
+    for i in range(n_img):
+        im_name = im_names[i]
+        part_name = part_names[i]
+        plt.subplot(1,n_img,i+1)
+        plot_Tslice(pulse, im_name, part_name, dim, shift=shift)
+
+def plot_Eslice_all(pulse, im_names, part_names, dim):
+    n_img = len(part_names)
+    plt.figure(figsize=(5.4*n_img+2, 5.4))
+    for i in range(n_img):
+        im_name = im_names[i]
+        part_name = part_names[i]
+        plt.subplot(1,n_img,i+1)
+        plot_Eslice(pulse, im_name, part_name, dim)
